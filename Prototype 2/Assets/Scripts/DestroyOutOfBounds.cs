b37d6a2a743e08a642c70474d15f84154ea19381
BURCU CINAR <burcu.alakus@gmail.com>
2022-11-14 22:26:07 +0300

    unit 2 lesson 4 docs

--- Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
+++ Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -10,19 +10,22 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
-    {
-        if(transform.position.z > topBound)
-        {
-            Destroy(gameObject);
-        } else if (transform.position.z < lowerBound)
+
+    {   // If an object goes past the players view in the game, remove that object
+        if (transform.position.z > topBound)
         {
             Destroy(gameObject);
         }
-        
+        else if (transform.position.z < lowerBound)
+        {
+            Debug.Log("Game Over!");
+            Destroy(gameObject);
+        }
+
     }
 }
