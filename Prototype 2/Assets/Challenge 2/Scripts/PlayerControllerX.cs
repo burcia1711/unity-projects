68aefa435067b357353b9cde3f6da07699f29568
BURCU CINAR <burcu.alakus@gmail.com>
2022-11-15 21:01:32 +0300

    unit 2 challenge 2 - play fetch docs

---
+++ Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    long lastSpawnMilisecond;
+    // Update is called once per frame
+    void Update()
+    {
+
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(DateTimeOffset.Now.ToUnixTimeMilliseconds() - lastSpawnMilisecond > 1000)
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                lastSpawnMilisecond = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+
+            }
+
+        }
+
+    }
+}
