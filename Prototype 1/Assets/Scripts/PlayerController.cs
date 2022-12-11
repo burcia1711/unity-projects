9d14aca15ea45e643159a6850ef3b3c96314a158
BURCU CINAR <burcu.alakus@gmail.com>
2022-12-11 10:12:37 +0300

    next steps: research and troubleshooting

--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -1,14 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float speed = 20.0f;
+    [SerializeField] float horsePower = 20.0f;
     [SerializeField] float turnSpeed = 45.0f;
     [SerializeField] float horizontalInput;
     [SerializeField] float forwardInput;
+    [SerializeField] GameObject centerOfMass;
+    [SerializeField] TextMeshProUGUI speedometerText;
+    [SerializeField] float speed;
+    [SerializeField] TextMeshProUGUI rpmText;
+    [SerializeField] float rpm;
+    private Rigidbody playerRb;
 
+    private void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        playerRb.centerOfMass = centerOfMass.transform.position;
+    }
+
+    private void Update()
+    {
+        speed = Mathf.Round(playerRb.velocity.magnitude * 2.237f);
+        speedometerText.SetText("Speed: " + speed + "mph");
+        rpm = Mathf.Round((speed % 30) * 40);
+        rpmText.SetText("RPM: " + rpm);
+    }
 
     // Update is called once per frame
     void FixedUpdate()
@@ -17,7 +37,7 @@ public class PlayerController : MonoBehaviour
         forwardInput =  Input.GetAxis("Vertical");
 
         //Move the vehicle forward based on the vertical input
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        playerRb.AddRelativeForce(Vector3.forward * forwardInput * horsePower);
         // Rotate the vehicle based on the horizontal input
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
